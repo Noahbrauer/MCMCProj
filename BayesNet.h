e5fd5818bce42606135fdbc7f40b1159865330ec
Noahbrauer <38707653+Noahbrauer@users.noreply.github.com>
2018-11-19 14:52:27 -0500

    Updated all map values for probabilities given
    1.2

--- BayesNet.h
+++ BayesNet.h
@@ -11,26 +11,27 @@ using namespace std;
 	The map is made with the values after the given bar of the probability all concatenated 
 	which is mapped to the probabilty for their result.
 	--Needs Work--
-	Find probabilties for new given functions. Current tables on program sheet are of normal
-	dependency, but we need tables based on markov blanket.
 	Possibly immplement function for MCMC sampling using the Markov Blanket 
 	concept (also called block Gibbs sampling).
 */
 class BayesNet {
+	
+	map<int, float> AgiveDBE = { {111, 0.50909},{110, 0.93333},{101, 0.80576},{100, 0.96137} };
 
-	map<int, int> AgiveDEB = { {111, },{110, },{101, },{100, },{011, },{010, },{001, },{000, } };
-	map<int, int> BgiveAEFC = { {1111, },{1110, },{1101, },{1100, },{1011, },{1010, },{1001, },{1000, }, 
-								{0111, },{0110, },{0101, },{0100, },{0011, },{0010, },{0001, },{0000, } };
-	map<int, int> CgiveBF = { {11, },{10, },{01, },{00, } };
-	map<int, int> DgiveA = { {1, },{0, } };
-	map<int, int> EgiveABGF = { {1111, },{1110, },{1101, },{1100, },{1011, },{1010, },{1001, },{1000, },
-								{0111, },{0110, },{0101, },{0100, },{0011, },{0010, },{0001, },{0000, } };
-	map<int, int> FgiveBCGE = { {1111, },{1110, },{1101, },{1100, },{1011, },{1010, },{1001, },{1000, },
-								{0111, },{0110, },{0101, },{0100, },{0011, },{0010, },{0001, },{0000, } };
-	map<int, int> GgiveEF = { {11, },{10, },{01, },{00, } };
+	map<int, float> BgiveACEF = { {1111, 0.10638},{1110, 0.21739},{1101, 0.61644},{1100, 0.78947},{1011, 0.40000},{1010, 0.40000},{1001, 0.36000},{1000, 0.90000},
+								  {0111, 0.32258},{0110, 0.52632},{0101, 0.74074},{0100, 0.86957},{0011, 0.14286},{0010, 0.72727},{0001, 0.50000},{0000, 0.94118} };
 
+	map<int, float> CgiveBF = { {11, 0.38462},{10, 0.13514},{01, 0.57143},{00, 0.14286} };
 
+	map<int, float> DgiveA = { {1, 0.80000},{0, 0.30000} };
 
+	map<int, float> EgiveGABF = { {1111, 0.00613},{1110, 0.01597},{1101, 0.19814},{1100, 0.75000},{1011, 0.17489},{1010, 0.75000},{1001, 0.69231},{1000, 0.94737} };
+
+	map<int, float> FgiveGBCE = { {1111, 0.20000},{1110, 0.66667},{1101, 0.05882},{1100, 0.32468},{1011, 0.42257},{1010, 0.82353},{1001, 0.50000},{1000, 0.88889} };
+
+	map<int, float> GgiveEF = { {11, 0.10000},{10, 0.40000},{01, 0.40000},{00, 0.20000} };
+
+	int mcmc();
 
 private:
 	bool A, B, C, D, E, F, G;
