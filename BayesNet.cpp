83d882b476f602b05127fbbad031337111398403
Noahbrauer <38707653+Noahbrauer@users.noreply.github.com>
2018-11-19 18:17:36 -0500

    Few changes to refine probabilities

--- BayesNet.cpp
+++ BayesNet.cpp
@@ -5,11 +5,11 @@ void BayesNet::initVars()
 {
 	srand(time(NULL));
 	A = rand() % 2;
-	B = rand()*rand() % 2;
-	C = rand()*rand()*rand() % 2;
+	B = rand() % 2;
+	C = rand() % 2;
 	D = 1;
-	E = rand()*rand()*rand()*rand() % 2;
-	F = rand()*rand()*rand()*rand()*rand() % 2;
+	E = rand() % 2;
+	F = rand()% 2;
 	G = 1;
 	cout << A << B << C << D << E << F << G << endl;
 }
@@ -28,23 +28,25 @@ int BayesNet::mcmc()
 	
 	// Generate new A value based on bias
 	float randNum = (rand() * rand()) % 100000 / 100000.0;
-	if (AgiveDBE[DBE] < randNum) {
+	if (AgiveDBE[DBE] > randNum) {
 		A = false;
 	}
 	else {
 		A = true;
 	}
+
 	// Generate new B value based on bias
 	randNum = (rand() * rand()) % 100000 / 100000.0;
-	if (BgiveACEF[ACEF] < randNum) {
+	if (BgiveACEF[ACEF] > randNum) {
 		B = false;
 	}
 	else {
 		B = true;
 	}
+
 	// Generate new C value based on bias
 	randNum = (rand() * rand()) % 100000 / 100000.0;
-	if (CgiveBF[BF] < randNum) {
+	if (CgiveBF[BF] > randNum) {
 		C = false;
 	}
 	else {
@@ -53,21 +55,21 @@ int BayesNet::mcmc()
 
 	// Generate new E value based on bias
 	randNum = (rand() * rand()) % 100000 / 100000.0;
-	if (EgiveGABF[GABF] < randNum) {
+	if (EgiveGABF[GABF] > randNum) {
 		E = false;
 	}
 	else {
 		E = true;
 	}
+
 	// Generate new F value based on bias
 	randNum = (rand() * rand()) % 100000 / 100000.0;
-	if (FgiveGBCE[GBCE] < randNum) {
+	if (FgiveGBCE[GBCE] > randNum) {
 		F = false;
 	}
 	else {
 		F = true;
 	}
-
 
 	if (B == true) {
 		return 1;
