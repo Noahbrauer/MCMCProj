3289880a6d36df41a7c4a2df2488456455ddcc31
Noahbrauer <38707653+Noahbrauer@users.noreply.github.com>
2018-11-19 16:51:33 -0500

    Created main to test in visual studio

--- BayeSource.cpp
+++ BayeSource.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
-
+#include "BayesNet.h"
 int main()
 {
-	
+	BayesNet net;
+	int numTrue = 0;
+	int record[10];
+	net.initVars();
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 1000; j++)
+		{
+			numTrue += net.mcmc();
+		}
+		record[i] = numTrue;
+		cout << numTrue << endl;
+		numTrue = 0;
+	}
+
+	system("PAUSE");
 }
