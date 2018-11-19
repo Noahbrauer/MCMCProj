357da984cb10ce0ccc346bc4f04b434d16976bcc
Noahbrauer <38707653+Noahbrauer@users.noreply.github.com>
2018-11-19 18:19:27 -0500

    Commenting

--- BayesNet.h
+++ BayesNet.h
@@ -2,26 +2,32 @@
 #include <iostream>
 #include <map>
 #include <time.h>
-
 using namespace std;
 
 /*
 	The class BayesNet holds in its private section our boolean values to be changed based on 
-	a biased input and the map variables computed using the Markov Blanket. In the public 
-	portion we declared a member function for randomly changing the boolean values held in the 
-	private data.
+	a biased input and the map variables computed using the Markov Blanket. We also declared a 
+	member function for randomly changing the boolean values held in the private data.
 */
 class BayesNet {
 public:
+	// initVars() is the function to initialize the variable contained in the class.
 	void initVars();
+	// mcmc() is the function that uses our probabilities to bias if a value is set to
+	// true or false. It uses probabilities of the given Bayesian Net that were computed 
+	// for each according to its Markov Blanket.
 	int mcmc();
 
 private:
+	// These variables are used to store the boolean values of the nodes in the net.
 	bool A, B, C, D, E, F, G;
-	map<int, float> AgiveDBE = { {111, 0.50909},{110, 0.93333},{101, 0.80576},{100, 0.96137} };
-	map<int, float> BgiveACEF = { {1111, 0.10638},{1110, 0.21739},{1101, 0.61644},{1100, 0.78947},{1011, 0.40000},{1010, 0.40000},{1001, 0.36000},{1000, 0.90000},
-								  {0111, 0.32258},{0110, 0.52632},{0101, 0.74074},{0100, 0.86957},{0011, 0.14286},{0010, 0.72727},{0001, 0.50000},{0000, 0.94118} };
-	map<int, float> CgiveBF = { {11, 0.38462},{10, 0.13514},{01, 0.57143},{00, 0.14286} };
-	map<int, float> EgiveGABF = { {1111, 0.00613},{1110, 0.01597},{1101, 0.19814},{1100, 0.75000},{1011, 0.17489},{1010, 0.75000},{1001, 0.69231},{1000, 0.94737} };
-	map<int, float> FgiveGBCE = { {1111, 0.20000},{1110, 0.66667},{1101, 0.05882},{1100, 0.32468},{1011, 0.42257},{1010, 0.82353},{1001, 0.50000},{1000, 0.88889} };
+	// Our map variables contain a map from given portion of the probability to the probability
+	// that the given case equates to true. The index is a binary value where each place holds 
+	// a vaule for the given boolean.
+	map<int, float> AgiveDBE = { {7, 0.50909},{6, 0.93333},{5, 0.80576},{4, 0.96137} };
+	map<int, float> BgiveACEF = { {15, 0.10638},{14, 0.21739},{13, 0.61644},{12, 0.78947},{11, 0.40000},{10, 0.40000},{9, 0.36000},{8, 0.90000},
+								  {7, 0.32258},{6, 0.52632},{5, 0.74074},{4, 0.86957},{3, 0.14286},{2, 0.72727},{1, 0.50000},{0, 0.94118} };
+	map<int, float> CgiveBF = { {3, 0.38462},{2, 0.13514},{1, 0.57143},{0, 0.14286} };
+	map<int, float> EgiveGABF = { {15, 0.00613},{14, 0.01597},{13, 0.19814},{12, 0.75000},{11, 0.17489},{10, 0.75000},{9, 0.69231},{8, 0.94737} };
+	map<int, float> FgiveGBCE = { {15, 0.20000},{14, 0.66667},{13, 0.05882},{12, 0.32468},{11, 0.42257},{10, 0.82353},{9, 0.50000},{8, 0.88889} };
 };
